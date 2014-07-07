1eba839a7a74cead13f366095b2d22cfb50af1ce
David Bruce Borenstein <david.bruce.borenstein@gmail.com>
2014-07-07 12:35:55 -0400

    WrapHelper1DTest

--- eslime/src/geometry/boundaries/helpers/WrapHelper1D.java
+++ eslime/src/geometry/boundaries/helpers/WrapHelper1D.java
@@ -19,53 +19,12 @@
  * /
  */
 
-/*
- *
- *  Copyright (c) 2014, David Bruce Borenstein and the Trustees of
- *  Princeton University.
- *
- *  Except where otherwise noted, this work is subject to a Creative Commons
- *  Attribution (CC BY 4.0) license.
- *
- *  Attribute (BY): You must attribute the work in the manner specified
- *  by the author or licensor (but not in any way that suggests that they
- *  endorse you or your use of the work).
- *
- *  The Licensor offers the Licensed Material as-is and as-available, and
- *  makes no representations or warranties of any kind concerning the
- *  Licensed Material, whether express, implied, statutory, or other.
- *
- *  For the full license, please visit:
- *  http://creativecommons.org/licenses/by/4.0/legalcode
- * /
- */
-
-/*
- *
- *  Copyright (c) 2014, David Bruce Borenstein and the Trustees of
- *  Princeton University.
- *
- *  Except where otherwise noted, this work is subject to a Creative Commons
- *  Attribution (CC BY 4.0) license.
- *
- *  Attribute (BY): You must attribute the work in the manner specified
- *  by the author or licensor (but not in any way that suggests that they
- *  endorse you or your use of the work).
- *
- *  The Licensor offers the Licensed Material as-is and as-available, and
- *  makes no representations or warranties of any kind concerning the
- *  Licensed Material, whether express, implied, statutory, or other.
- *
- *  For the full license, please visit:
- *  http://creativecommons.org/licenses/by/4.0/legalcode
- * /
- */
-
 package geometry.boundaries.helpers;
 
 import control.identifiers.Coordinate;
 import control.identifiers.Flags;
 import geometry.lattice.Lattice;
+import geometry.shape.Line;
 import geometry.shape.Rectangle;
 import geometry.shape.Shape;
 
@@ -79,13 +38,13 @@ public class WrapHelper1D extends WrapHelper {
     public WrapHelper1D(Shape shape, Lattice lattice) {
         super(shape, lattice);
 
-        if (shape.getClass() != Rectangle.class) {
-            throw new UnsupportedOperationException("WrapHelper2D only supports Rectangle arenas.");
+        if (shape.getClass() != Line.class) {
+            throw new UnsupportedOperationException("WrapHelper1D only supports Line arenas.");
         }
 
-        Rectangle rect = (Rectangle) shape;
+        Line line = (Line) shape;
 
-        length = rect.getDimensions()[0];
+        length = line.getDimensions()[0];
     }
 
     public Coordinate wrapAll(Coordinate toWrap) {
@@ -129,7 +88,7 @@ public class WrapHelper1D extends WrapHelper {
 
     @Override
     protected void checkValid(Coordinate toWrap) {
-        if (toWrap.x() != 0 || toWrap.y() != 0) {
+        if (toWrap.x() != 0 || toWrap.z() != 0) {
             throw new IllegalStateException("WrapHelper1D requires strictly 0 x and z values.");
         }
     }
--- eslime/tests/geometry/boundaries/helpers/WrapHelper1DTest.java
+++ eslime/tests/geometry/boundaries/helpers/WrapHelper1DTest.java
@@ -21,11 +21,76 @@
 
 package geometry.boundaries.helpers;
 
-import junit.framework.TestCase;
+import control.identifiers.Coordinate;
+import geometry.lattice.Lattice;
+import geometry.lattice.LinearLattice;
+import geometry.lattice.RectangularLattice;
+import geometry.shape.Line;
+import geometry.shape.Rectangle;
+import geometry.shape.Shape;
+import test.EslimeTestCase;
 
-public class WrapHelper1DTest extends TestCase {
+public class WrapHelper1DTest extends EslimeTestCase {
 
-    public void testNothing() {
-        fail("Implement me! Copy some tests from WrapHelper2DTest.");
+    private WrapHelper1D query;
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        Lattice lattice = new LinearLattice();
+        Shape shape = new Line(lattice, 5);
+        query = new WrapHelper1D(shape, lattice);
+    }
+
+    public void testWrapAll() throws Exception {
+        Coordinate toWrap = new Coordinate(0, 5, 0);
+        Coordinate expected = new Coordinate(0, 0, 0);
+        Coordinate actual = query.wrapAll(toWrap);
+        assertEquals(expected, actual);
+    }
+
+    public void testXWrap() throws Exception {
+        boolean thrown = false;
+
+        try {
+            query.xWrap(null);
+        } catch (UnsupportedOperationException ex) {
+            thrown = true;
+        }
+
+        assertTrue(thrown);
+    }
+
+    public void testYWrap() throws Exception {
+        // Positive out of bounds Y coordinate
+        Coordinate toWrap = new Coordinate(0, 6, 0);
+        Coordinate expected = new Coordinate(0, 1, 0);
+        Coordinate actual = query.yWrap(toWrap);
+        assertEquals(expected, actual);
+
+        // Negative out of bounds Y coordinate
+        toWrap = new Coordinate(0, -1, 0);
+        expected = new Coordinate(0, 4, 0);
+        actual = query.yWrap(toWrap);
+        assertEquals(expected, actual);
+    }
+
+    public void testZWrap() throws Exception {
+        boolean thrown = false;
+
+        try {
+            query.zWrap(null);
+        } catch (UnsupportedOperationException ex) {
+            thrown = true;
+        }
+
+        assertTrue(thrown);
+    }
+
+    public void testAllInBounds() throws Exception {
+        Coordinate toWrap = new Coordinate(0, 3, 0);
+        Coordinate expected = new Coordinate(0, 3, 0);
+        Coordinate actual = query.wrapAll(toWrap);
+        assertEquals(expected, actual);
     }
 }
