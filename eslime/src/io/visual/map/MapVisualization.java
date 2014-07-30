2272f29ad94924831814d357c782e0908792c73b
David Bruce Borenstein <david.bruce.borenstein@gmail.com>
2014-07-30 11:34:23 -0400

    Revert "981770-75904400 Change copyright to all rights reserved" (Somehow, this badness got checked in twice)
    This reverts commit 1809f49c5dcfe72c66f5c2d01fd8f41ba10c1ffe.

--- eslime/src/io/visual/map/MapVisualization.java
+++ eslime/src/io/visual/map/MapVisualization.java
@@ -19,27 +19,6 @@
  * /
  */
 
-/*
- *
- *  Copyright (c) 2014, David Bruce Borenstein and the Trustees of
- *  Princeton University.
- *
- *  Except where otherwise noted, this work is subject to a Creative Commons
- *  Attribution (CC BY 4.0) license.
- *
- *  Attribute (BY): You must attribute the work in the manner specified
- *  by the author or licensor (but not in any way that suggests that they
- *  endorse you or your use of the work).
- *
- *  The Licensor offers the Licensed Material as-is and as-available, and
- *  makes no representations or warranties of any kind concerning the
- *  Licensed Material, whether express, implied, statutory, or other.
- *
- *  For the full license, please visit:
- *  http://creativecommons.org/licenses/by/4.0/legalcode
- * /
- */
-
 package io.visual.map;
 
 import control.identifiers.Coordinate;
